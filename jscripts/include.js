f5ed230f9c77e97863031727fa12cb45e25cac4e
guillaume.riflet <guillaume.riflet@962b3d77-9c2c-0410-b289-6ffed1588a1c>
2008-02-17 21:03:50 +0000

    Found a new, better and safer version of the JS injection method.

--- jscripts/include.js
+++ jscripts/include.js
@@ -1,9 +1,21 @@
-function include(script_filename) {
-    var html_head = document.getElementsByTagName('head').item(0);
-    var js = document.createElement('script');
-    js.setAttribute('language', 'javascript');
-    js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', script_filename);
-    html_head.appendChild(js);
-    return false;
+/** include - including .js files from JS - bfults@gmail.com - 2005-02-09    **
+ ** Code licensed under Creative Commons Attribution-ShareAlike License      **
+ ** http://creativecommons.org/licenses/by-sa/2.0/                           **/
+var hIncludes = null;
+function include(sURI) {
+  if (document.getElementsByTagName) {
+    if (!hIncludes) {
+      hIncludes = {};
+      var cScripts = document.getElementsByTagName("script");
+      for (var i=0,len=cScripts.length; i < len; i++)
+        if (cScripts[i].src) hIncludes[cScripts[i].src] = true;
+    }
+    if (!hIncludes[sURI]) {
+      var oNew = document.createElement("script");
+      oNew.type = "text/javascript";
+      oNew.src = sURI;
+      hIncludes[sURI]=true;
+      document.getElementsByTagName("head")[0].appendChild(oNew);
+    }
+  }
 }
